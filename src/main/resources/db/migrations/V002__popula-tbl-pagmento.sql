03946d99b53139b5fc9297a23fbcbce66197858f
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-04-29 14:55:50 -0300

    :sparkles: feat: Criados scripts de banco de dados

---
+++ src/main/resources/db/migrations/V002__popula-tbl-pagmento.sql
@@ -0,0 +1,10 @@
+insert into pagamento(codigo, cpf, cnpj, numero_cartao, tipo_pagamento, valor, status) values
+                                                                                           (1, '68155138070', '52841266000160', '5313804251588402', 'CARTAO_CREDITO', 100, 'SUCESSO'),
+                                                                                           (2, '57660841025', '56494417000102', '5189522327039264', 'CARTAO_CREDITO', 200, 'SUCESSO'),
+                                                                                           (3, '88009926094', '35120657000177', '4916901860402019', 'CARTAO_CREDITO', 400, 'SUCESSO'),
+                                                                                           (4, '72135995002',             null,               null,            'PIX', 500, 'PENDENTE'),
+                                                                                           (5, '31252911009', '89127604000107',               null,            'PIX', 300, 'FALHA'),
+                                                                                           (6, '66924793045',             null, '5275615521919557', 'CARTAO_CREDITO', 200, 'SUCESSO'),
+                                                                                           (7, '02382659009', '29809989000160', '4916901860402019', 'CARTAO_CREDITO', 100, 'SUCESSO'),
+                                                                                           (8, '19315713095',             null, '4916901860402019', 'CARTAO_CREDITO', 100, 'SUCESSO'),
+                                                                                           (9, '88009926094', '47121436000100',               null,            'PIX', 600, 'FALHA');
